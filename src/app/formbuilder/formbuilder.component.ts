3aa38befbfd1309aa81ecb266b26719157240d23
Abhishek <abhi.ghosh27feb@gmail.com>
2022-02-11 20:39:51 +0530

    add

--- src/app/formbuilder/formbuilder.component.ts
+++ src/app/formbuilder/formbuilder.component.ts
@@ -8,17 +8,13 @@ import {FormBuilder,FormGroup,Validators} from '@angular/forms'
 export class FormbuilderComponent implements OnInit {
   userForms:FormGroup
   
-  /*userForm= new FormGroup({name:new FormControl("abhishek",Validators.required),
-  email:new FormControl(),address:new FormGroup({city:new FormControl(),
-    postelCode:new FormControl()
-  ,street:new FormControl()
-  })})*/
-  constructor(private _formBuilder:FormBuilder) { 
-
+  constructor(private fb:FormBuilder) { }
+  registrationForm=this.fb.group({name:['rahul',Validators.required]})
+  ngOnInit(): void {
   }
+  onSubmit(){
 
-  ngOnInit() {
-    this._formBuilder.group({name:['abhishek',,[Validators.required]]})
+    console.log("value",this.registrationForm.value)
   }
 
 }
